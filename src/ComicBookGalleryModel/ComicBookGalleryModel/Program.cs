1b796b2d70891cec0f47be18c3dd7486cb8bab56
Joel Brennan <joelbrennan0@gmail.com>
2017-10-15 20:59:05 +0100

    Finished the LINQ query section

--- src/ComicBookGalleryModel/ComicBookGalleryModel/Program.cs
+++ src/ComicBookGalleryModel/ComicBookGalleryModel/Program.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Data.Entity;
+using System.Diagnostics;
 
 namespace ComicBookGalleryModel
 {
@@ -13,22 +14,42 @@ namespace ComicBookGalleryModel
         static void Main(string[] args)
         {
             using (var context = new Context())
-            {              
-                var comicBooks = context.ComicBooks
+            {
+                Debug.WriteLine("Starting App...");
+                context.Database.Log = (message) => Debug.WriteLine(message);
+
+                var comicBookId = 1;
+
+                var comicBook = context.ComicBooks
+                    .Where(cb => cb.Id == comicBookId)
                     .Include(cb => cb.Series)
-                    .Include(cb => cb.Artists.Select(a => a.Artist))
-                    .Include(cb => cb.Artists.Select(a => a.Role))
-                    .ToList();
+                    .SingleOrDefault();
 
-                foreach (var comicBook in comicBooks)
-                {
-                    var artistRoleNames = comicBook.Artists
-                        .Select(a => $"{a.Artist.Name} - {a.Role.Name}").ToList();
-                    var artistRolesDisplayText = string.Join(", ", artistRoleNames);
+                Console.WriteLine(comicBook.DisplayText);
 
-                    Console.WriteLine(comicBook.DisplayText);
-                    Console.WriteLine(artistRolesDisplayText);
-                }
+                //var comicBook1 = context.ComicBooks.Find(comicBookId);
+                //var comicBook2 = context.ComicBooks.Find(comicBookId);
+
+                //var comicBooks = context.ComicBooks
+                //    //.Include(cb => cb.Series)
+                //    //.Include(cb => cb.Artists.Select(a => a.Artist))
+                //    //.Include(cb => cb.Artists.Select(a => a.Role))
+                //    .ToList();
+
+                //foreach (var comicBook in comicBooks)
+                //{
+                //    if(comicBook.Series == null)
+                //    {
+                //        context.Entry(comicBook).Reference(cb => cb.Series)
+                //            .Load();
+                //    }
+                //    var artistRoleNames = comicBook.Artists
+                //        .Select(a => $"{a.Artist.Name} - {a.Role.Name}").ToList();
+                //    var artistRolesDisplayText = string.Join(", ", artistRoleNames);
+
+                //    Console.WriteLine(comicBook.DisplayText);
+                //    Console.WriteLine(artistRolesDisplayText);
+                //}
 
                 Console.ReadLine();
             }
